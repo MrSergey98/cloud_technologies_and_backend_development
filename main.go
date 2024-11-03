a0ca6e5e47a0eae5951d32071ee1f69af6cd231d
MrSergey <mrsergey98@mail.ru>
2024-11-03 22:07:16 +0300

    feat: Main logic

--- main.go
+++ main.go
@@ -2,41 +2,96 @@ package main
 
 import (
 	//"encoding/json"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Handler struct {
 }
 
-func get() {
-
+type Users struct {
+    Users []User `json:"users"`
 }
 
-func post(buf []byte) {
+var users Users
 
+type User struct {
+	Username string `json:"username"`
 }
 
-func delete(buf []byte) {
+func  Get() string {
+	//TODO: возвращать ответ
+	jsonResponse, err := json.Marshal(users)
+	if err != nil {
+		return ""
+	}
+	return string(jsonResponse)
+}
 
+func Post(body []byte) {
+	var user User
+	if err := json.Unmarshal(body, &user); err != nil {
+		fmt.Println(err)
+		return
+	}
+	users.Users = append(users.Users, user)
+	//TODO: write data in file
+}
+
+func Delete(body []byte) {
+	var user User
+	if err := json.Unmarshal(body, &user); err != nil {
+		fmt.Println(err)
+		return
+	}
+	var index int
+	for i, inMemoryUser := range users.Users {
+		if inMemoryUser.Username == user.Username {index = i}
+	}
+	//TODO: if index  == nil {}
+	users.Users = append(users.Users[:index], users.Users[index+1:]...)
+	//TODO: write data in file
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 
-	buf, err := io.ReadAll(r.Body)
+	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
-	fmt.Println(string(buf))
+	switch request_method := r.Method; request_method{
+		case "GET":
+			//TODO: возвращать ответ
+			Get()
+		case "POST":
+			Post(body)
+		case "DELETE":
+			Delete(body)
+	}
+	fmt.Print(r.Method)
+	
 
 }
 
 func main() {
+	var jsonFile, err = os.Open("users.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer jsonFile.Close()
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	json.Unmarshal(byteValue, &users)
 	var h Handler
 	s := &http.Server{
 		Addr:    ":8080",
