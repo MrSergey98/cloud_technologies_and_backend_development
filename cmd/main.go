9016552fb3d8b2c3a387ed33333063550e1ae75c
MrSergey <mrsergey98@mail.ru>
2024-12-13 19:14:27 +0300

    feat(LR6): updated main.go and go.mod files

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-    "fmt"
-    "github.com/MrSergey98/cloud_technologies_and_backend_development/mymodule"
+	"fmt"
+
+	"github.com/MrSergey98/cloud_technologies_and_backend_development/mymodule"
 )
 
 func main() {
-    message := mymodule.Hello("World")
-    fmt.Println(message)
+	message := mymodule.Hello("World")
+	fmt.Println(message)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module mymodule
+module github.com/MrSergey98/cloud_technologies_and_backend_development
 
 go 1.23.2
